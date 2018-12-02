de61df208f148ba5c5658e1aa7e70d598df32153
Michael Lenghel <michael.lenghel@gmail.com>
2018-12-02 17:38:42 +0000

    Added more fuctionality to definition lookup

--- hangman/app/src/main/java/com/example/pcmasterrace/hangman/GameOverActivity.java
+++ hangman/app/src/main/java/com/example/pcmasterrace/hangman/GameOverActivity.java
@@ -1,25 +1,42 @@
 package com.example.pcmasterrace.hangman;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.SQLException;
 
 public class GameOverActivity extends AppCompatActivity {
 
     TextView heading;
     TextView scoreText;
+    TextView definition;
     Intent intent;
     String userName;
+    String userWord;
     int userScore;
+    int gameStatus;
     DataManager db;
+    String DEBUG_TAG = "find_error";
 
 
     @Override
@@ -28,15 +45,16 @@ public class GameOverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_over);
         heading = (TextView) findViewById(R.id.gameOverHeading);
         scoreText = (TextView) findViewById(R.id.totalScore);
+        definition = (TextView) findViewById(R.id.definition);
 
 
-        //Won = 1, Lose = 0
-        int gameStatus= getIntent().getIntExtra("status",0);//0 is default value
+                //Won = 1, Lose = 0
+        gameStatus= getIntent().getIntExtra("status",0);//0 is default value
         userScore = getIntent().getIntExtra("score",0);//0 is default value
 
         //Get the word to display if user lost
         Bundle extras = getIntent().getExtras();
-        String userWord = "";
+        userWord = "";
         if(extras !=null) {
             userWord = extras.getString("word", "");
             userName = extras.getString("userName", "");
@@ -54,13 +72,136 @@ public class GameOverActivity extends AppCompatActivity {
         }
 
         scoreText.setText(scoreDisplay);
+
+
+        //Display definition of word on screen
+        getDef();
+
     }
+
+    public void getDef()
+    {
+        String defUrl = "https://www.dictionary.com/browse/" + userWord;
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo     = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            new DownloadWebpageTask().execute(defUrl);
+        }
+        else
+        {
+            definition.setText("No definitions available");
+        }
+    }
+
+    /* Use AsyncTask to create a task away from the main UI thread. This task takes a URL string and uses it to create an HttpUrlConnection. Once the connection has been established, the AsyncTask downloads the contents of the webpage as an InputStream. Finally, the InputStream is converted into a string, which is displayed in the UI by the AsyncTask's onPostExecute method*/
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String>
+    {
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try
+            {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        protected void onPostExecute(String result)
+        {
+
+            definition.setText(result);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+
+    private String downloadUrl(String myurl) throws IOException
+    {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        }
+        finally
+        {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    // Reads an InputStream and converts it to a String.
+    // Reads an InputStream and converts it to a String.
+    private String readIt(InputStream is)
+    {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try
+        {
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line).append('\n');
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
 
     /** Called when the user touches the button */
     public void startHangman(View view)
     {
         intent = new Intent(getBaseContext(), PlayingHangman.class);
-        intent.putExtra("score", userScore);
+
+
+        if (gameStatus == 1)
+        {
+            // If user has won they can keep incrementing their score
+            intent.putExtra("score", userScore);
+        }
+        else
+        {
+            // If user has lost we reset the score back to zero
+            intent.putExtra("score", 0);
+        }
+        intent.putExtra("userName", userName);
         startActivity(intent);
     }
 
@@ -74,8 +215,6 @@ public class GameOverActivity extends AppCompatActivity {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
         long val = db.insertScore(userName, Integer.toString(userScore));
     }
 
